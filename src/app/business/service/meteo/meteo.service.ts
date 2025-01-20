ac59d6d4ce0d191a88ee9b3dc61231d063bdec14
LA4 <andr.ludow.com>
2025-01-20 13:13:36 +0100

    service spotify

--- src/app/business/service/meteo/meteo.service.ts
+++ src/app/business/service/meteo/meteo.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MeteoService {
-
-  constructor() { }
+  constructor() {}
+  public getDataMeteo(town: string) {}
 }
