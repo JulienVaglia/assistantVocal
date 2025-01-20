fb8fbc1aeb743e3d6741669879cb62b372b39866
JulienVaglia <julien.vaglia@gmail.com>
2025-01-20 22:07:15 +0100

    token+recherche ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,6 @@ import { LoginComponent } from './components/login/login/login.component';
 export const routes: Routes = [
 
     { path: '', component: LoginComponent },
+    { path: 'spotifySearch', component: LoginComponent },
 
 ];
